9c1330d22d62a519e1ebcf6de838a910a2fd7590
等陌上花开 <zhzhn2002@gmail.com>
2024-03-10 13:30:04 +0800

    新增组件管理器，用于插件与主程序之间安全获取组件。

---
+++ src/main/java/container/desktop/containerdesktopbackend/BackendBeanManager.java
@@ -0,0 +1,21 @@
+package container.desktop.containerdesktopbackend;
+
+import container.desktop.api.BeanManager;
+import container.desktop.containerdesktopbackend.listener.ContextInitializer;
+
+public class BackendBeanManager implements BeanManager {
+    @Override
+    public <T> T getBean(Class<T> requiredType) {
+        return ContextInitializer.getContext().getBean(requiredType);
+    }
+
+    @Override
+    public Object getBean(String name) {
+        return ContextInitializer.getContext().getBean(name);
+    }
+
+    @Override
+    public <T> T getBean(String name, Class<T> requiredType) {
+        return ContextInitializer.getContext().getBean(name, requiredType);
+    }
+}
