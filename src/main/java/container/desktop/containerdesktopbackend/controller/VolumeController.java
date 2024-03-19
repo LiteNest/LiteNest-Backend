69c05540cee1fc71701702aad70c7b6a8f9ef7d1
等陌上花开 <zhzhn2002@gmail.com>
2024-03-19 15:23:38 +0800

    新增了创建卷的功能

---
+++ src/main/java/container/desktop/containerdesktopbackend/controller/VolumeController.java
@@ -0,0 +1,36 @@
+package container.desktop.containerdesktopbackend.controller;
+
+import com.alibaba.fastjson2.JSONObject;
+import container.desktop.api.service.VolumeService;
+import container.desktop.containerdesktopbackend.DTO.VolumeCreationDTO;
+import container.desktop.containerdesktopbackend.Result;
+import container.desktop.containerdesktopbackend.entity.BackendVolume;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/volumes")
+public class VolumeController {
+
+    private final VolumeService<BackendVolume> volumeService;
+
+    public VolumeController(@Qualifier("volume_service") VolumeService<BackendVolume> volumeService) {
+        this.volumeService = volumeService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Result> create(@RequestBody VolumeCreationDTO volumeCreationDTO) {
+        String name = volumeService.create(volumeCreationDTO.size());
+
+        Result result = Result.builder()
+                .code(HttpStatus.CREATED.value())
+                .message(name)
+                .build();
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+}
